c503fac4ea6730256380e33f9c73ecc66ce6e6c8
mehmet ozek <mehmetozek@mehmets-MacBook-Air.local>
2020-01-19 15:25:19 -0600

    exercise 2

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'; //{useState}
-import './/Person/Person.css';
+import './Person/Person.css';
 import './App.css';
 import Person from'./Person/Person';
 
@@ -62,7 +62,8 @@ class App extends Component {
   render() {
     
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -85,12 +86,21 @@ class App extends Component {
         })}
       </div> 
       )
+      style.backgroundColor = 'red'
+    }
+    
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red')
+    } 
+    if (this.state.persons.length <= 1) {
+      classes.push('bold')
     }
     
     return (
       <div className = "App">
         <h1>Hi, I'm React App </h1>
-        <p>This is really working</p>
+        <p className={classes.join(' ')}>This is really working</p>
         
         <button 
         style = {style}
